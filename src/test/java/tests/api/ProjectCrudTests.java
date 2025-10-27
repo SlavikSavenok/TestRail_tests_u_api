7afa4c3633e829cb181c573d10be1858a7c352ce
SlavikSavenok <savenokslava@gmail.com>
2025-10-27 22:13:56 +0300

    api_crud_read_project_test

--- src/test/java/tests/api/ProjectCrudTests.java
+++ src/test/java/tests/api/ProjectCrudTests.java
@@ -9,16 +9,32 @@ public class ProjectCrudTests extends BaseApiTest {
 
     @Test(testName = "API Post проверка создания проекта")
     public void addProjectTest() {
-
         Project expectedProject = new Project();
         expectedProject.setName("CRUD Test Project");
         expectedProject.setAnnouncement("CRUD Announcement");
         expectedProject.setShowAnnouncement(false);
 
         actualProject = projectService.addProject(expectedProject);
+
+        Assert.assertNotNull(actualProject.getId());
         Assert.assertEquals(actualProject.getName(), expectedProject.getName());
         Assert.assertEquals(actualProject.getAnnouncement(), expectedProject.getAnnouncement());
         Assert.assertEquals(actualProject.getShowAnnouncement(), expectedProject.getShowAnnouncement());
+    }
+
+    @Test(testName = "API Get проверка чтения созданного проекта",
+            description = "Тест на проверку чтения созданного проекта по ID",
+            dependsOnMethods = "addProjectTest")
+    public void getProjectTest() {
+        int createdProjectId = actualProject.getId();
+
+        Project retrievedProject = projectService.getProject(createdProjectId);
+
+        Assert.assertEquals(retrievedProject.getId(),actualProject.getId());
+        Assert.assertEquals(retrievedProject.getName(),actualProject.getName());
+        Assert.assertEquals(retrievedProject.getAnnouncement(),actualProject.getAnnouncement());
+        Assert.assertEquals(retrievedProject.getShowAnnouncement(),actualProject.getShowAnnouncement());
+
     }
 
     @Test(testName = "API Post проверка обновления проекта", description = "Тест на проверку обновления проекта",
