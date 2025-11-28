8a9cb87ea01793156f9229450f0062ce399c609d
SlavikSavenok <savenokslava@gmail.com>
2025-11-28 15:40:13 +0300

    commit

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,19 @@
 package pages;
 
-public class LoginPage {
+import baseEntities.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    private final By EMAIL_INPUT_LOCATOR = By.id("name");
+    //private final By PASSWORD_INPUT_LOCATOR =;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected By getPageIdentifier() {
+        return EMAIL_INPUT_LOCATOR;
+    }
 }
---
+++ src/test/java/tests/gui/gui.java
@@ -0,0 +1,14 @@
+package tests.gui;
+
+
+import baseEntities.BaseGuiTest;
+import org.testng.annotations.Test;
+
+
+public class gui extends BaseGuiTest {
+@Test()
+    public void test(){
+
+}
+
+}
